518013617f18f0c6fd855454bf29a880a0167d6d
VenkateshMogili <mogilivenkatesh3@gmail.com>
2023-05-02 15:17:06 +0530

    Initial Commit

---
+++ Ausper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ausper
+//
+//  Created by DoodleBlue on 02/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
